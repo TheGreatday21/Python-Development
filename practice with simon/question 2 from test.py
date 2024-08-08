a8f8dfad150f79f33d6eb39f36580a8b47319d1f
TheGreatday21 <kaizzielijah@gmail.com>
2024-08-08 18:47:51 +0300

    practice makes perfect

--- practice with simon/question 2 from test.py
+++ practice with simon/question 2 from test.py
@@ -35,7 +35,7 @@ else:
 for i in range (1,11):
     a = list((f"UCU100{i}"))
     print(a)
-'''
+
 
 
 #4. Tuples, Sets, & Dictionaries:
@@ -62,6 +62,67 @@ dict_values = dictionary_one.values()
 print(dict_values)
 #the whole dictionary is
 print(dictionary_one)
+
+
+#python time counter
+import time#import the time library
+def plank(secs):#create a function that counts
+    print("Alright less get it ")
+
+    while secs > 0 :#use  a while loop to make the code loop
+        print(f"Youare left with {secs}  seconds to go ")
+        secs -= 1
+        time.sleep(1)
+a = int(input("Place in your plank time in seconds : "))
+plank(a) 
+print("Hurrayyyyy you did it .. less go again ")
+
+
+string_text =  "Hello world!"
+print(string_text[6:])#this calls all the letters from the sixth index of the string
+
+string= "python is fun !"
+print(string[::-1])#reverses the text literally
+
+
+##getting multiples of 3 and 5 form user input number
+numbers =  int(input("Type in any numeber here : "))
+print(f"Your number is {numbers}\n")
+for num in range(0,numbers +1):#create a for loop with a range to pick the numberes from
+    if num % 3 == 0:#if the number in numbers has a zero remainder after division with 3
+        print("Fizz")
+    elif num % 5 == 0:#if the number in numbers has a zero remainder after division with 5
+        print("Buzz")
+    elif num % 3 == 0 and num % 5 ==0:#if the number in numbers has a zero remainder after division with both  3 and 5
+        print("FizzBuzz")
+    else:#if the number fills none of the above critea just print the number live and shit
+        print(num)
+
+
+
+## multiplication
+import time
+print("\n\tThis program will create a multiplication table for any number you input\n")
+time.sleep(2)
+multiplication = int(input("Input any number yuo want to get the multiplication for "))
+
+for i in range(1,multiplication+1):
+    print(f"{multiplication} * {i} = {multiplication * i}")
+    time.sleep(1)
+'''
+
+##summation of all even numbers
+number1 = int(input("Input number : "))
+count = 0
+for i in range(1,number1+1):
+    if i % 2 ==0:
+        print(i)
+        count += 1
+print(f"There are {count} even numbers in your number{number1}")
+
+
+
+
 
 
 
