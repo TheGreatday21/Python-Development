52cec8b193b96d12662ce1ffd8b996e09c924c08
TheGreatday21 <kaizzielijah@gmail.com>
2024-09-26 23:50:51 +0300

    .

---
+++ free_lancing/tribe_selector.py
@@ -0,0 +1,15 @@
+#we first create  a list of the varios foods eaten by the people
+food = ["sweet potatoes","Matooke", "Irish potatoes", "Rice"]
+#we create anotherlist of the tribes of the people
+tribe = ['Basoga', 'Baganda', 'Banyoro','Bahima']
+#we then create a dictionary with the data we have recieved 
+
+
+
+
+
+
+
+
+
+
