e3545f16ef0f50b81c2399c6b930fd1ead4050ae
TheGreatday21 <kaizzielijah@gmail.com>
2024-10-04 00:51:12 +0300

    algos

--- free_lancing/class_notes/linear_algortihm.py
+++ free_lancing/class_notes/linear_algortihm.py
@@ -1,11 +1,12 @@
-def linear_search(array,target):#Array is fo rthe elements present and the target is the element we are looking for in the array#
+def linear_search(array,target):#Array is for the elements present and the target is the element we are looking for in the array#
 	for i in range(len(array)): 
 		if array[i] == target:
 			return i
-	return -1 #standard way of saying e=what you looked for doesnt exist #
+	return -1 #standard way of saying what you looked for does not exist #
 data = [ 3,2,5,7,8,1]
 target = 8
 result = linear_search(data, target)
+#Putting if statements for the result we are to get
 if result !=-1:
 	print(f"the element is at index {result}")
 else:
