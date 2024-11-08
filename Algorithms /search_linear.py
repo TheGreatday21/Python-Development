e3fae2346e2b21cd96a2cc6fc6d16affe63dbf75
TheGreatday21 <kaizzielijah@gmail.com>
2024-11-08 14:35:23 +0300

    notebooks

---
+++ Algorithms /search_linear.py
@@ -0,0 +1,11 @@
+def search(array,target) :
+    for i in range (len(array)):
+        if array[i] == target:
+            return i
+    return -1
+data = [ 2,3,5,7,8,9]
+new = 5
+
+result  = search(data, new)
+
+print(f"Your number {new} is at index {result}")
