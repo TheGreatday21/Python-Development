e2c05319077f2d28dce2590f61a9c0cb10558ede
TheGreatday21 <kaizzielijah@gmail.com>
2025-09-29 21:06:29 +0300

    reviewing test work

--- Learning/year2/test.py
+++ Learning/year2/test.py
@@ -1,15 +1,28 @@
-##Closures - Functions that remember where they are from 
-def my_fun(name):
-    def my_fun_2(okay):
-        return f"Hey there {name} i hope you have a good {okay}"
-    return my_fun_2
-my_fun
+class Book:
+    def __init__(self,title,author,year,price):
+        self.title = title
+        self.author = author
+        self.year= year 
+        self.price = price
+        
+    def discount(self):
+        if  self.price >= 10000:
+            d_sum = self.price * (10/100)
+            self.price -= d_sum
+            print(f"Discount: {d_sum} newprice: {self.price}")
+            
+        else:
+            d_sum = self.price * (2/100)
+            self.price -= d_sum
+            print(f"Discount: {d_sum} newprice: {self.price}")
+             
+def main():
 
-
-stu_1 = my_fun("Elijah")
-stu_2 = my_fun("James")
-
-print(my_fun("time"))
-print(my_fun("day"))
-
-
+    me1 = Book("lido","DC",1998,15000)
+    
+    me1.price = 40000
+    
+    me1.discount()
+        
+if __name__ == "__main__":
+    main()
